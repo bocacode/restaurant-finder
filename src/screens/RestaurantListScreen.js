a98a36fde5a5f39dbd91600ed2ea3d3647e27c24
jonathans199 <jso1919@gmail.com>
2022-02-23 17:28:55 -0500

    added pull down to refresh

--- src/screens/RestaurantListScreen.js
+++ src/screens/RestaurantListScreen.js
@@ -1,11 +1,5 @@
 import React, { useEffect, useState, useCallback } from 'react'
-import {
-     Text,
-     View,
-     ScrollView,
-     RefreshControl,
-     ActivityIndicator,
-} from 'react-native'
+import { Text, View, ScrollView, RefreshControl, ActivityIndicator } from 'react-native'
 import RestaurantCard from '../components/RestaurantCard'
 
 import { Button } from 'react-native-elements'
@@ -13,65 +7,64 @@ import { useNavigation } from '@react-navigation/native'
 import { withSafeAreaInsets } from 'react-native-safe-area-context'
 
 function RestaurantList() {
-     const [restaurants, setRestaurants] = useState()
-     const [refreshing, setRefreshing] = useState(false)
+  const [restaurants, setRestaurants] = useState()
+  const [refreshing, setRefreshing] = useState(false)
 
-     useEffect(() => {
-          fetch('https://bocacode-intranet-api.web.app/restaurants')
-               .then(response => response.json())
-               .then(data => setRestaurants(data))
-               .catch(alert)
-     }, [restaurants])
+  const wait = timeout => {
+    return new Promise(resolve => setTimeout(resolve, timeout))
+  }
 
-     const onRefresh = useCallback(() => {
-          setRefreshing(true)
-          withSafeAreaInsets(2000).then(() => {
-               setRefreshing(false)
-          })
-     })
+  useEffect(() => {
+    getRestaurantsFromApi()
+  }, [restaurants])
 
-     const navigation = useNavigation()
+  const getRestaurantsFromApi = () => {
+    fetch('https://bocacode-intranet-api.web.app/restaurants')
+      .then(response => response.json())
+      .then(data => (setRestaurants(data), setRefreshing(false)))
+      .catch(alert)
+  }
 
-     const goToNewRestaurant = () => {
-          navigation.navigate('NewRestaurant')
-     }
+  const onRefresh = useCallback(() => {
+    setRefreshing(true)
+    getRestaurantsFromApi()
+    wait(2000).then(() => setRefreshing(false))
+  }, [])
 
-     return (
-          <View>
-               <Button
-                    title="Add New Restaurant"
-                    onPress={goToNewRestaurant}
-                    buttonStyle={{
-                         backgroundColor: '#666',
-                         borderRadius: 30,
-                    }}
-                    containerStyle={{
-                         alignSelf: 'center',
-                         width: 200,
-                         marginHorizontal: '50%',
-                         marginVertical: 10,
-                    }}
-               />
+  const navigation = useNavigation()
 
-               {!restaurants ? (
-                    <ActivityIndicator /> 
-               ) : (
-                    <ScrollView
-                         refreshControl={onRefresh}
-                         refreshing={refreshing}
-                    >
-                         {restaurants.map(restaurant => {
-                              return (
-                                   <RestaurantCard
-                                        key={restaurant.id}
-                                        restaurant={restaurant}
-                                   />
-                              )
-                         })}
-                    </ScrollView>
-               )}
-          </View>
-     )
+  const goToNewRestaurant = () => {
+    navigation.navigate('NewRestaurant')
+  }
+
+  return (
+    <View>
+      <Button
+        title='Add New Restaurant'
+        onPress={goToNewRestaurant}
+        buttonStyle={{
+          backgroundColor: '#666',
+          borderRadius: 30,
+        }}
+        containerStyle={{
+          alignSelf: 'center',
+          width: 200,
+          marginHorizontal: '50%',
+          marginVertical: 10,
+        }}
+      />
+
+      {!restaurants ? (
+        <ActivityIndicator />
+      ) : (
+        <ScrollView refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
+          {restaurants.map(restaurant => {
+            return <RestaurantCard key={restaurant.id} restaurant={restaurant} />
+          })}
+        </ScrollView>
+      )}
+    </View>
+  )
 }
 
 export default RestaurantList
